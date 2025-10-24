43bdccea2ba7315513e0c5442f838ac170c45143
yeonhuinamgoong <yeonhui.namgoong@gmail.com>
2025-10-24 16:18:16 +0900

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_KEY: string; // ✅ optional(?) 아님
+  readonly VITE_API_KEY: string; // 반드시 있어야 한다고 강제
 }
 
 interface ImportMeta {
